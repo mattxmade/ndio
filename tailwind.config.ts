bd598cea639caad7ff6897a5e00885d676d387be
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-03 17:17:08 +0100

    Update tailwind.config: add theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,14 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      background: "#000",
+      primary: "#f9f9f9",
+      splash: "#e30b5d",
+      muted: "#a1a1a9",
+      "muted-dark": "#273444",
+    },
+
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
