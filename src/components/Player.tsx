d0c790863517d4048d8a21b129fad9696a8917a1
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-03 16:15:12 +0100

    Create initial layout; implement tw util classes; add placeholder content

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,8 +1,19 @@
 const Player = () => {
   return (
-    <div>
-      <h2>Player</h2>
-    </div>
+    <section
+      id="player"
+      className="flex justify-between fixed bottom-0 w-screen z-20 py-5 pl-8 bg-black"
+    >
+      <div id="player-metadata">
+        <img src="cover.avif" alt="cover art" className="w-16" />
+      </div>
+
+      <div id="player-controls">
+        <img src="icons/player/play.svg" className="w-9" />
+      </div>
+
+      <div id="player-tools"></div>
+    </section>
   );
 };
 
