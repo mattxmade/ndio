2d221d83e83c69308b8991af15f462ed0b2dbf43
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-04 22:34:43 +0100

    Update Component layout

--- src/components/core/Aside.tsx
+++ src/components/core/Aside.tsx
@@ -2,39 +2,37 @@ import { HomeIcon, MusicIcon, HeartIcon, UserIcon } from "lucide-react";
 
 const Aside = (props: React.ComponentProps<"aside">) => {
   return (
-    <aside className="relative w-80 bg-background" {...props}>
-      <div className="fixed h-full bg-inherit">
-        <nav className="flex flex-col gap-9 px-5 pt-8">
-          <a
-            href="/"
-            className="flex items-center gap-3 text-muted text-sm hover:text-primary"
-          >
-            <HomeIcon className="w-6 stroke-primary" />
-            Discover
-          </a>
-          <a
-            href="#"
-            className="flex items-center gap-3 text-muted text-sm hover:text-primary"
-          >
-            <MusicIcon className="w-6 stroke-primary" />
-            My Creations
-          </a>
-          <a
-            href="#"
-            className="flex items-center gap-3 text-muted text-sm hover:text-primary"
-          >
-            <HeartIcon className="w-6 stroke-primary" />
-            Liked Songs
-          </a>
-          <a
-            href="#"
-            className="flex items-center gap-3 text-muted text-sm hover:text-primary"
-          >
-            <UserIcon className="w-6 stroke-primary" />
-            Following
-          </a>
-        </nav>
-      </div>
+    <aside {...props}>
+      <nav className="w-[16rem] flex flex-col gap-9 px-5 pt-8">
+        <a
+          href="/"
+          className="flex items-center gap-3 text-muted text-sm hover:text-primary"
+        >
+          <HomeIcon className="w-6 stroke-primary" />
+          Discover
+        </a>
+        <a
+          href="#"
+          className="flex items-center gap-3 text-muted text-sm hover:text-primary"
+        >
+          <MusicIcon className="w-6 stroke-primary" />
+          My Creations
+        </a>
+        <a
+          href="#"
+          className="flex items-center gap-3 text-muted text-sm hover:text-primary"
+        >
+          <HeartIcon className="w-6 stroke-primary" />
+          Liked Songs
+        </a>
+        <a
+          href="#"
+          className="flex items-center gap-3 text-muted text-sm hover:text-primary"
+        >
+          <UserIcon className="w-6 stroke-primary" />
+          Following
+        </a>
+      </nav>
     </aside>
   );
 };
