b27ec74c9d74d5c6b2a10837c9d892d3b139b1b3
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-05 23:02:05 +0100

    Update Button.tsx

--- src/components/core/Button.tsx
+++ src/components/core/Button.tsx
@@ -3,7 +3,7 @@
 const Button = (props: React.ComponentPropsWithRef<"button">) => {
   return (
     <button
-      className="text-sm py-[0.6rem] px-4 rounded duration-100 bg-splash hover:bg-opacity-80"
+      className="text-sm w-fit py-[0.6rem] px-4 rounded duration-100 bg-splash hover:bg-opacity-80"
       {...props}
     />
   );
