80ac1c7ad778d3f4aacac0f8863cfd6f2ae13fed
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-05 17:39:15 +0100

    Create Component; define types; initial commit

---
+++ src/components/song/Metadata.tsx
@@ -0,0 +1,11 @@
+import { PlayIcon, HeartIcon, Clock1Icon } from "lucide-react";
+
+type MetadataProps = {
+  skipDate: boolean;
+} & React.ComponentPropsWithRef<"section">;
+
+const Metadata = (props: MetadataProps) => {
+  return <section />;
+};
+
+export default Metadata;
