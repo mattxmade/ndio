e2bac1cd74f7080e4ead979358dac8639a5d6104
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-05 22:33:26 +0100

    Implement useState; create handlers + render conditions; add aria-labels

--- src/components/player/PlayButton.tsx
+++ src/components/player/PlayButton.tsx
@@ -1,12 +1,18 @@
 "use client";
 
 import { PlayIcon, PauseIcon } from "lucide-react";
+import { useState } from "react";
 
 type PlayButtonProps = {
   variant: "overlay" | "player" | "standalone";
 } & React.ComponentPropsWithRef<"button">;
 
 const PlayButton = ({ variant, ...props }: PlayButtonProps) => {
+  const [hovering, setHovering] = useState(false);
+
+  const handlePointerOver = () => setHovering(true);
+  const handlePointerOut = () => setHovering(false);
+
   return variant === "player" ? (
     <button
       aria-label="Play/Pause track"
@@ -16,13 +22,22 @@ const PlayButton = ({ variant, ...props }: PlayButtonProps) => {
     </button>
   ) : variant === "overlay" ? (
     <button
-      className="absolute w-full h-full p-4 flex items-center justify-center bg-primary"
+      aria-label="Play/Pause track"
+      className="absolute inset-0 p-4 flex items-center justify-center bg-transparent duration-150 hover:bg-background hover:bg-opacity-70"
+      onPointerOver={handlePointerOver}
+      onPointerOut={handlePointerOut}
       {...props}
     >
-      <PlayIcon size={48} className="stroke-primary" />
+      <PlayIcon
+        size={48}
+        className={`${
+          !hovering ? "stroke-transparent" : "stroke-primary opacity-70"
+        } duration-150`}
+      />
     </button>
   ) : (
     <button
+      aria-label="Play/Pause track"
       className="p-4 flex items-center justify-center rounded-full bg-primary duration-300 hover:scale-110"
       {...props}
     >
