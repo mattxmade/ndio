926dab65b2d341ecb9fa3ff274812d9092db5c3b
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-05 01:30:50 +0100

    Add logo image element + social icons; update layout

--- src/components/core/Aside.tsx
+++ src/components/core/Aside.tsx
@@ -1,9 +1,21 @@
 import { HomeIcon, MusicIcon, HeartIcon, UserIcon } from "lucide-react";
 
+import {
+  TwitterIcon,
+  InstagramIcon,
+  DiscordIcon,
+  TikTokIcon,
+  RedditIcon,
+} from "../Icons";
+
 const Aside = (props: React.ComponentProps<"aside">) => {
   return (
     <aside {...props}>
-      <nav className="w-[16rem] flex flex-col gap-9 px-5 pt-8">
+      <a href="/">
+        <img src="logo.svg" alt="logo" className="w-40 pt-1 pl-5" />
+      </a>
+
+      <nav className="w-[16rem] flex flex-col gap-9 px-4 pt-10">
         <a
           href="/"
           className="flex items-center gap-3 text-muted text-sm hover:text-primary"
@@ -33,6 +45,28 @@ const Aside = (props: React.ComponentProps<"aside">) => {
           Following
         </a>
       </nav>
+
+      <section className="absolute w-full p-2 bottom-24 flex flex-col items-center gap-4 bg-background">
+        <p className="text-xs text-muted">AI Music Generator</p>
+
+        <div className="flex gap-4">
+          <a href="#">
+            <TwitterIcon className="w-5 h-5 fill-muted" />
+          </a>
+          <a href="#">
+            <InstagramIcon className="w-5 h-5 fill-muted" />
+          </a>
+          <a href="#">
+            <DiscordIcon className="w-5 h-5 fill-muted" />
+          </a>
+          <a href="#">
+            <TikTokIcon className="w-5 h-5 fill-muted" />
+          </a>
+          <a href="#">
+            <RedditIcon className="w-5 h-5 fill-muted" />
+          </a>
+        </div>
+      </section>
     </aside>
   );
 };
