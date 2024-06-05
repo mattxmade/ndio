b393fe79dc47ecc6fe5832776b51c10073a6fc2f
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-05 18:22:53 +0100

    Complete initial layout; provision props; add types

--- src/components/song/Metadata.tsx
+++ src/components/song/Metadata.tsx
@@ -1,11 +1,40 @@
-import { PlayIcon, HeartIcon, Clock1Icon } from "lucide-react";
+import { PlayIcon, HeartIcon, ClockIcon, LucideProps } from "lucide-react";
 
 type MetadataProps = {
+  size?: LucideProps["size"];
   skipDate: boolean;
-} & React.ComponentPropsWithRef<"section">;
+  metadata: { plays: number; likes: number; created: number };
+} & React.ComponentPropsWithRef<"ul">;
 
 const Metadata = (props: MetadataProps) => {
-  return <section />;
+  const { skipDate, metadata } = props;
+
+  return (
+    <ul className="flex gap-4" {...props}>
+      <li className="flex gap-1 items-center">
+        <i role="presentation">
+          <PlayIcon size={props.size ?? 12} className="fill-primary" />
+        </i>
+        <p>{metadata.plays}</p>
+      </li>
+
+      <li className="flex gap-2 items-center">
+        <i role="presentation">
+          <HeartIcon size={props.size ?? 12} className="fill-primary" />
+        </i>
+        <p>{metadata.likes}</p>
+      </li>
+
+      {skipDate ? null : (
+        <li className="flex gap-1 items-center">
+          <i role="presentation">
+            <ClockIcon size={props.size ?? 12} className="stroke-primary" />
+          </i>
+          <p>{metadata.created}</p>
+        </li>
+      )}
+    </ul>
+  );
 };
 
 export default Metadata;
