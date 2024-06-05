b271874b1b67dded098a73be2ba37f85a1eabe60
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-05 02:25:09 +0100

    Complte initial input prompt layout

--- src/components/generator/GeneratorForm.tsx
+++ src/components/generator/GeneratorForm.tsx
@@ -1,12 +1,28 @@
+import { DicesIcon, ZapIcon } from "lucide-react";
+
 const GeneratorForm = () => {
   return (
     <form className="py-3">
-      <fieldset title="input prompt">
+      <fieldset title="input prompt" className="relative">
+        <div className="absolute w-full h-full pl-3 pr-4 flex items-center justify-between">
+          <ZapIcon className=" fill-muted stroke-muted" />
+
+          <div className="flex gap-3">
+            <button aria-label="random song prompt">
+              <DicesIcon className="w-5 duration-300 origin-right hover:rotate-45" />
+            </button>
+
+            <button disabled={true} className="py-2 px-4 rounded bg-rose-500">
+              Create
+            </button>
+          </div>
+        </div>
+
         <input
           type="text"
           name="input-prompt"
           placeholder="a song about..."
-          className="p-4 pl-10 rounded border-[1px] border-fuchsia-900 bg-transparent"
+          className="w-full p-4 pl-11 rounded border-[1px] border-fuchsia-900 bg-transparent"
         />
       </fieldset>
     </form>
