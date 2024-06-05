b4806fa4df514417b994805670c46cbf6f8f25a6
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-05 23:15:08 +0100

    Implement Card Component

--- src/app/songs/[id]/page.tsx
+++ src/app/songs/[id]/page.tsx
@@ -1,7 +1,13 @@
+import Card from "@/components/song/Card";
+
+import { songData } from "@/content";
+
 export default function SongPage() {
   return (
-    <main className="flex flex-col p-3 pt-6">
-      <h1>Song Page</h1>
+    <main className="flex flex-col p-3 pt-6 bg-background">
+      <Card songData={songData} />
+
+      <section className="flex flex-wrap"></section>
     </main>
   );
 }
