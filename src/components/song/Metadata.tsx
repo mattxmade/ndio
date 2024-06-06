07b5fdbda02d43de504b3a33723a0b29a9f88df9
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-06 19:44:44 +0100

    Update Metadata.tsx

--- src/components/song/Metadata.tsx
+++ src/components/song/Metadata.tsx
@@ -2,7 +2,7 @@ import { PlayIcon, HeartIcon, ClockIcon, LucideProps } from "lucide-react";
 
 type MetadataProps = {
   size?: LucideProps["size"];
-  skipDate: boolean;
+  skipDate?: boolean;
   metadata: { plays: number; likes: number; created: number };
 } & React.ComponentPropsWithRef<"ul">;
 
@@ -10,17 +10,17 @@ const Metadata = (props: MetadataProps) => {
   const { skipDate, metadata } = props;
 
   return (
-    <ul className="flex gap-4" {...props}>
+    <ul {...props} className={`flex gap-4 ` + props.className}>
       <li className="flex gap-1 items-center">
         <i role="presentation">
-          <PlayIcon size={props.size ?? 12} className="fill-primary" />
+          <PlayIcon size={props.size ?? 12} />
         </i>
         <p className={!props.size ? "text-xs" : "text-sm"}>{metadata.plays}</p>
       </li>
 
       <li className="flex gap-2 items-center">
         <i role="presentation">
-          <HeartIcon size={props.size ?? 12} className="fill-primary" />
+          <HeartIcon size={props.size ?? 12} />
         </i>
         <p className={!props.size ? "text-xs" : "text-sm"}>{metadata.likes}</p>
       </li>
@@ -28,7 +28,7 @@ const Metadata = (props: MetadataProps) => {
       {skipDate ? null : (
         <li className="flex gap-1 items-center">
           <i role="presentation">
-            <ClockIcon size={props.size ?? 12} className="stroke-primary" />
+            <ClockIcon size={props.size ?? 12} />
           </i>
           <p className={!props.size ? "text-xs" : "text-sm"}>
             {metadata.created}
