877d7be4c4279f1078cdfa876ab9a5e109b14197
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-07 21:34:26 +0100

    Create React Component; initial commit

---
+++ src/components/search/SortOptions.tsx
@@ -0,0 +1,9 @@
+type SortOptionsProps = {
+  sortBy: { item: string; initSelect?: boolean }[];
+} & React.ComponentPropsWithRef<"ul">;
+
+const SortOptions = ({ sortBy, ...props }: SortOptionsProps) => {
+  return <ul {...props}></ul>;
+};
+
+export default SortOptions;
