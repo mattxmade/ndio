677fea9816e395eeb9ed95eb351db23f9528e486
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-07 02:12:27 +0100

    Create initial layout; implement PlayButton; assign props

--- src/components/song/CardGraphic.tsx
+++ src/components/song/CardGraphic.tsx
@@ -1,9 +1,25 @@
+import PlayButton from "../player/PlayButton";
+
 type CardGrpahicProps = {
   songData: SongData;
 } & React.ComponentPropsWithRef<"section">;
 
 const CardGraphic = ({ songData, ...props }: CardGrpahicProps) => {
-  return <section {...props}></section>;
+  return (
+    <section className="flex" {...props}>
+      <div className="flex gap-4">
+        <div className="relative h-fit">
+          <img
+            src={songData.cover}
+            alt={songData.title}
+            className="aspect-square w-24 max-w-none"
+          />
+
+          <PlayButton variant="overlay" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default CardGraphic;
