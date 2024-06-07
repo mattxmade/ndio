8fc21a7bb4b25114ea98d9d1658560134be7aa35
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-07 03:07:21 +0100

    Expand Carousel implementation; add h2 as prop; update layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,15 @@ import { songGroups } from "@/content";
 
 export default function Home() {
   return (
-    <main className="grid p-7 pt-3 mt-3 bg-background mb-36 rounded">
+    <main className="grid gap-5 mt-3 mb-36 pt-3 pr-3 pb-7 pl-[1.4rem] rounded bg-background  ">
       {songGroups.categories.map((category) => (
-        <Carousel key={category.id} categoryData={category} />
+        <Carousel
+          key={category.id}
+          categoryData={category}
+          className="overflow-hidden grid gap-2"
+        >
+          <h2 className="text-2xl text-primary">{category.title}</h2>
+        </Carousel>
       ))}
     </main>
   );
