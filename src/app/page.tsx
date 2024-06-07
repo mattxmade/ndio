1e4f64295db7f188b3978dd1b2b42826d8c688a1
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-07 01:43:19 +0100

    Map group data; implement Carousel Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import { playlist } from "@/content";
+import Carousel from "@/components/layout/Carousel";
+import { songGroups } from "@/content";
 
 export default function Home() {
   return (
     <main className="flex flex-col p-7 pt-3 mt-3 bg-background mb-36 rounded">
-      <section></section>
+      {songGroups.categories.map((category) => (
+        <Carousel key={category.id} categoryData={category} />
+      ))}
     </main>
   );
 }
