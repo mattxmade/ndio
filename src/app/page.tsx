60f9b2caa75da7335c4f7f9ade94683461b6801e
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-07 02:14:56 +0100

    Update layout: flex => grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { songGroups } from "@/content";
 
 export default function Home() {
   return (
-    <main className="flex flex-col p-7 pt-3 mt-3 bg-background mb-36 rounded">
+    <main className="grid p-7 pt-3 mt-3 bg-background mb-36 rounded">
       {songGroups.categories.map((category) => (
         <Carousel key={category.id} categoryData={category} />
       ))}
