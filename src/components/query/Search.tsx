c17ae8acc3122b7781f6669035f86f06fd604553
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-08 21:48:31 +0100

    Implement SearchBar + SortOptions; create header layout; add directive

--- src/components/query/Search.tsx
+++ src/components/query/Search.tsx
@@ -1,22 +1,43 @@
+"use client";
+
 import useQuery from "./useQuery";
 import CardDetail from "../song/CardDetail";
+import SearchBar from "./SearchBar";
+import SortOptions from "./SortOptions";
 
 type SearchProps = {
   items: SongData[];
-} & React.ComponentPropsWithRef<"ul">;
+} & React.ComponentPropsWithRef<"section">;
 
 const Search = ({ items, ...props }: SearchProps) => {
   const { results } = useQuery(items);
 
-  <section className="grid gap-2">
-    {props.children}
+  return (
+    <section {...props}>
+      <header className="flex flex-wrap items-center justify-between">
+        {props.children}
 
-    <ul className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3" {...props}>
-      {results.map((item, item_i) => (
-        <li key={"row-list-item" + item_i}>{<CardDetail songData={item} />}</li>
-      ))}
-    </ul>
-  </section>;
+        <div className="w-full flex items-center justify-between">
+          <SearchBar />
+          <SortOptions
+            sortBy={[
+              { item: "Trending", initSelect: true },
+              { item: "Popular" },
+              { item: "Recent" },
+            ]}
+          />
+        </div>
+      </header>
+
+      <ul className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3">
+        {results.map((item, item_i) => (
+          <li key={"row-list-item" + item_i}>
+            {<CardDetail songData={item} />}
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 export default Search;
