0f1e23cdc48c243cb5abd902bef00230db930345
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-08 02:16:32 +0100

    Map data; Complete initial layout

--- src/components/search/SortOptions.tsx
+++ src/components/search/SortOptions.tsx
@@ -4,9 +4,22 @@ type SortOptionsProps = {
 
 const SortOptions = ({ sortBy, ...props }: SortOptionsProps) => {
   return (
-    <ul {...props}>
+    <ul
+      className="flex gap mt-3 mb-2 p-1 rounded bg-muted-dark bg-opacity-55"
+      {...props}
+    >
       {sortBy.map((option, i) => (
-        <li key={i}>{option.item}</li>
+        <li key={i}>
+          <button
+            className={
+              option.initSelect
+                ? "p-2 px-3 text-xs text-primary-muted rounded bg-black"
+                : "p-2 px-3 text-xs text-muted"
+            }
+          >
+            {option.item}
+          </button>
+        </li>
       ))}
     </ul>
   );
