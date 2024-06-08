704eeb0738f1d034dea9cfbe71c462372be4f244
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-08 17:42:25 +0100

    Implement useState; create handler function

--- src/components/query/useQuery.ts
+++ src/components/query/useQuery.ts
@@ -1,5 +1,16 @@
 "use client";
 
-const useQuery = () => {};
+import { useState } from "react";
+
+const useQuery = () => {
+  const [results, setResults] = useState();
+
+  const handleQuery = async (sortBy: SortOption, search?: string) => {};
+
+  return {
+    results,
+    handleQuery,
+  };
+};
 
 export default useQuery;
