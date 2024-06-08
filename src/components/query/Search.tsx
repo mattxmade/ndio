cb353adbb5262eb9a3b249c7259f6c6e08a4b8ce
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-08 21:29:23 +0100

    Implement useQuery; create layout; map data

--- src/components/query/Search.tsx
+++ src/components/query/Search.tsx
@@ -1,5 +1,22 @@
-const Search = () => {
-  <section className="grid gap-2">Search View</section>;
+import useQuery from "./useQuery";
+import CardDetail from "../song/CardDetail";
+
+type SearchProps = {
+  items: SongData[];
+} & React.ComponentPropsWithRef<"ul">;
+
+const Search = ({ items, ...props }: SearchProps) => {
+  const { results } = useQuery(items);
+
+  <section className="grid gap-2">
+    {props.children}
+
+    <ul className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3" {...props}>
+      {results.map((item, item_i) => (
+        <li key={"row-list-item" + item_i}>{<CardDetail songData={item} />}</li>
+      ))}
+    </ul>
+  </section>;
 };
 
 export default Search;
