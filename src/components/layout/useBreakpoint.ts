1757d752d169626b7f7054cd1efdf479e76944ae
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-09 14:42:14 +0100

    Complete useBreakpoint hook; provision state change on breakpoint match

--- src/components/layout/useBreakpoint.ts
+++ src/components/layout/useBreakpoint.ts
@@ -1,20 +1,45 @@
+"use client";
+
 import { styleConfig } from "@/app/style.config";
 import { useEffect, useState } from "react";
 
 const { sm, md, lg, xl } = styleConfig.screens;
 
 const breakpoints = {
-  sm: Number(sm.slice(0, -3)),
-  md: Number(md.slice(0, -3)),
-  lg: Number(lg.slice(0, -3)),
-  xl: Number(xl.slice(0, -3)),
-  "2xl": Number(styleConfig.screens["2xl"].slice(0, -3)),
+  sm: Number(sm.slice(0, -2)),
+  md: Number(md.slice(0, -2)),
+  lg: Number(lg.slice(0, -2)),
+  xl: Number(xl.slice(0, -2)),
+  "2xl": Number(styleConfig.screens["2xl"].slice(0, -2)),
 };
 
 const useBreakpoint = () => {
-  const [currentBreakpoint, setCurrentBreakpoint] = useState();
+  const [currentBreakpoint, setCurrentBreakpoint] =
+    useState<keyof typeof breakpoints>("sm");
 
-  const handleResize = () => {};
+  const handleResize = () => {
+    if (window.innerWidth < breakpoints.md) setCurrentBreakpoint("sm");
+
+    if (
+      window.innerWidth >= breakpoints.md &&
+      window.innerWidth < breakpoints.lg
+    )
+      setCurrentBreakpoint("md");
+
+    if (
+      window.innerWidth >= breakpoints.lg &&
+      window.innerWidth < breakpoints.xl
+    )
+      setCurrentBreakpoint("lg");
+
+    if (
+      window.innerWidth >= breakpoints.xl &&
+      window.innerWidth < breakpoints["2xl"]
+    )
+      setCurrentBreakpoint("xl");
+
+    if (window.innerWidth >= breakpoints["2xl"]) setCurrentBreakpoint("2xl");
+  };
 
   useEffect(() => {
     window.addEventListener("resize", handleResize);
@@ -23,6 +48,10 @@ const useBreakpoint = () => {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
+
+  return {
+    currentBreakpoint,
+  };
 };
 
 export default useBreakpoint;
