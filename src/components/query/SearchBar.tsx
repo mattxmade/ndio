40a9677d27c2b0aa5963e0a02deb1bed9871a911
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-09 12:39:59 +0100

    Style input; add icon button; define types

--- src/components/query/SearchBar.tsx
+++ src/components/query/SearchBar.tsx
@@ -1,7 +1,20 @@
-const SearchBar = () => {
+import { XIcon } from "lucide-react";
+
+type SearchBarProps = {} & React.ComponentPropsWithRef<"input">;
+
+const SearchBar = ({ ...props }: SearchBarProps) => {
   return (
-    <form>
-      <input type="text" placeholder="search..." />
+    <form className="relative w-1/2 ">
+      <button className="absolute inset top-1/2 right-1 translate-x-[-50%] translate-y-[-50%]">
+        <XIcon size={18} />
+      </button>
+
+      <input
+        type="text"
+        placeholder="Search..."
+        className="text-xs text-primary placeholder:text-xs w-full p-2 px-4 rounded-lg border-[1px] border-muted-dark border-opacity-55 bg-transparent"
+        {...props}
+      />
     </form>
   );
 };
