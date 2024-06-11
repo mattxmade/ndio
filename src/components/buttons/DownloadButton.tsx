f2a6d1a9ac35b12d4439fd1b17e7973f899cf41f
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-12 00:51:47 +0100

    Create Component; provison type props; initial commit

---
+++ src/components/buttons/DownloadButton.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { LucideProps, DownloadIcon } from "lucide-react";
+
+import IconButton from "./IconButton";
+
+type DownloadButtonProps = {
+  iconProps: LucideProps;
+} & React.ComponentPropsWithRef<"button">;
+
+const DownloadButton = ({ iconProps, ...props }: DownloadButtonProps) => {
+  return (
+    <IconButton {...props}>
+      <DownloadIcon {...iconProps} />
+    </IconButton>
+  );
+};
+
+export default DownloadButton;
