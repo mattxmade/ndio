9c2ffd24ef9a51e3b5cea8d029ea8a503cc3b629
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-11 18:49:32 +0100

    Implement PageTemplate Component

--- src/app/songs/[id]/page.tsx
+++ src/app/songs/[id]/page.tsx
@@ -3,6 +3,7 @@ import Card from "@/components/song/Card";
 import CardDetail from "@/components/song/CardDetail";
 
 import { styleConfig } from "@/app/style.config";
+import PageTemplate from "@/components/layout/PageTemplate";
 
 const heightValue = 28; // 7rem 112px
 const twHeight = styleConfig["height"][heightValue];
@@ -15,42 +16,46 @@ export default function SongPage() {
   const playlistHeight = playlistItems.length * Number(twHeight.slice(0, -3));
 
   return (
-    <main className="flex flex-col p-7 pt-3 mt-3 bg-background mb-36 rounded">
-      <Card songData={songData} />
+    <PageTemplate variant="sm">
+      <main className="flex flex-col p-7 pt-3 mt-3 bg-background mb-36 rounded">
+        <Card songData={songData} className="flex flex-col md:flex-row gap-4" />
 
-      <section className="flex flex-wrap p-1 pt-10">
-        <section className="md:w-1/2 ">
-          <h2 className="text-xl font-bold mb-4">Lyrics</h2>
+        <section className="flex flex-wrap p-1 pt-10">
+          <section className="md:w-1/2 ">
+            <h2 className="text-lg md:text-xl font-bold mb-4">Lyrics</h2>
 
-          {lyrics.map((line) => (
-            <p
-              key={line}
-              className={`my-2 ${line.endsWith("]") ? "mt-4 font-bold" : ""}`}
-            >
-              {line}
-            </p>
-          ))}
-        </section>
-
-        <section className="flex flex-col gap-2 md:w-1/2">
-          <h2 className="text-xl font-bold">More from this creator</h2>
-
-          <ul
-            className={
-              playlistHeight > maxHeight
-                ? "pl-2 pr-4 pb-6 overflow-y-scroll"
-                : "pl-2 pr-4"
-            }
-            style={{ height: `calc(${twHeight} * ${3.5})` }}
-          >
-            {playlistItems.map((_, i) => (
-              <li key={i} className="h-28">
-                <CardDetail songData={songData} />
-              </li>
+            {lyrics.map((line) => (
+              <p
+                key={line}
+                className={`text-xs lg:text-base my-2 ${
+                  line.endsWith("]") ? "mt-4 font-bold" : ""
+                }`}
+              >
+                {line}
+              </p>
             ))}
-          </ul>
+          </section>
+
+          <section className="flex flex-col gap-2 md:w-1/2">
+            <h2 className="text-xl font-bold">More from this creator</h2>
+
+            <ul
+              className={
+                playlistHeight > maxHeight
+                  ? "pl-2 pr-4 pb-6 overflow-y-scroll"
+                  : "pl-2 pr-4"
+              }
+              style={{ height: `calc(${twHeight} * ${3.5})` }}
+            >
+              {playlistItems.map((_, i) => (
+                <li key={i} className="h-28">
+                  <CardDetail songData={songData} />
+                </li>
+              ))}
+            </ul>
+          </section>
         </section>
-      </section>
-    </main>
+      </main>
+    </PageTemplate>
   );
 }
