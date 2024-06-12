b0bdc31cd73d13691e7337f5d2c7ceb27d92b0bc
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-12 16:18:04 +0100

    Create small/medium+ layouts; add buttons + modal Components

--- src/components/song/Card.tsx
+++ src/components/song/Card.tsx
@@ -1,6 +1,11 @@
-import Button from "../core/Button";
 import Metadata from "./Metadata";
+import MenuModal from "./MenuModal";
+import Button from "../core/Button";
+import MenuButton from "../buttons/MenuButton";
 import PlayButton from "../player/PlayButton";
+import LikeButton from "../buttons/LikeButton";
+import ShareButton from "../buttons/ShareButton";
+import DownloadButton from "../buttons/DownloadButton";
 
 type CardProps = {
   songData: SongData;
@@ -8,18 +13,20 @@ type CardProps = {
 
 const Card = ({ songData, ...props }: CardProps) => {
   return (
-    <section className="flex gap-4" {...props}>
-      <div className="relative h-fit">
+    <section {...props} className={"" + " " + props.className}>
+      <MenuModal />
+
+      <div className="relative w-fit h-fit self-center md:self-start">
         <img
           src={songData.cover}
           alt={songData.title}
-          className="aspect-square w-[18.8rem] max-w-none"
+          className="aspect-square w-52 md:w-[18.8rem] max-w-none"
         />
 
         <PlayButton variant="overlay" />
       </div>
 
-      <section className="flex flex-col gap-2">
+      <section className="relative pt-4 md:pt-0 flex flex-col gap-2">
         <div className="flex gap-2">
           <div className="w-7 h-7 rounded-full bg-blue-950" />
           <h2 className="text-sm text-muted">{songData.author.username}</h2>
@@ -29,29 +36,70 @@ const Card = ({ songData, ...props }: CardProps) => {
           {songData.title}
         </h1>
 
-        <div className="flex flex-col gap-[1.1rem] mb-2">
-          <p className="text-sm text-muted font-bold">
-            Prompt: {songData.prompt}
-          </p>
-          <p className="text-sm text-muted">
-            {songData.tags.map((tag, i, array) =>
-              i !== array.length - 1 ? `${tag}, ` : tag
-            )}
-          </p>
-        </div>
+        <p className="hidden md:block text-sm text-muted font-bold">
+          Prompt: {songData.prompt}
+        </p>
 
-        <Button>Sign in to create more tracks like this!</Button>
+        <div className="flex gap-2 flex-col-reverse md:flex-col">
+          {/* Tags + CTA */}
+          <div className="grid gap-2">
+            <ul className="flex gap-3 md:gap-1 mt-4 md:mt-2 mb-6 md:mb-2 text-sm text-muted">
+              {songData.tags.map((tag, i, array) => (
+                <li key={"tag-" + i + " " + tag}>
+                  {i !== array.length - 1 ? (
+                    <>
+                      <a className="px-3 md:px-0 py-4 md:py-0 rounded-3xl md:rounded-none md:p-0 text-sm text-muted cursor-pointer bg-muted-dark md:bg-background bg-opacity-55 md:bg-opacity-100 hover:underline">
+                        {tag}
+                      </a>
+                      <span className="hidden md:inline">, </span>
+                    </>
+                  ) : (
+                    <>
+                      <a className="px-3 py-4 rounded-3xl md:rounded-none md:p-0 text-sm text-muted cursor-pointer bg-muted-dark md:bg-background bg-opacity-55 md:bg-opacity-100 hover:underline">
+                        {tag}
+                      </a>
+                    </>
+                  )}
+                </li>
+              ))}
+            </ul>
 
-        <div className="flex gap-8 mt-3 mb-5">
-          <PlayButton variant="standalone" />
+            <Button className="w-full md:w-fit">
+              Sign in to create more tracks like this!
+            </Button>
+          </div>
 
-          <div className="flex gap-4">
-            {/* <ShareButton/> */}
-            {/* <DownloadButton/> */}
+          {/* Interactivity + Metadata */}
+          <div className="flex flex-row-reverse justify-between md:grid md:gap-2">
+            <div className="flex items-center gap-4 mt-3 mb-5">
+              <PlayButton
+                variant="standalone"
+                className="absolute top-0 right-0 p-2 md:relative"
+              />
+
+              <div className="flex">
+                <LikeButton
+                  aria-label="Like track"
+                  className="block md:hidden"
+                  iconProps={{ className: "fill-primary stroke-primary" }}
+                />
+                <ShareButton aria-label="Share track" />
+                <DownloadButton
+                  aria-label="Download track"
+                  className="hidden md:block"
+                />
+                <MenuModal>
+                  <MenuButton
+                    aria-label="Track menu"
+                    className="block md:hidden"
+                  />
+                </MenuModal>
+              </div>
+            </div>
+
+            <Metadata skipDate size={13} metadata={songData.metadata} />
           </div>
         </div>
-
-        <Metadata skipDate size={13} metadata={songData.metadata} />
       </section>
     </section>
   );
