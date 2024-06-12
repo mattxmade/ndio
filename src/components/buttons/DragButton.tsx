1390beeb2f79ecf1c1c641be889c6285149bb081
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-12 22:13:02 +0100

    Create Button Component; initial commit

---
+++ src/components/buttons/DragButton.tsx
@@ -0,0 +1,12 @@
+import { MinusIcon } from "lucide-react";
+import IconButton from "./IconButton";
+
+const DragButton = () => {
+  return (
+    <IconButton>
+      <MinusIcon />
+    </IconButton>
+  );
+};
+
+export default DragButton;
