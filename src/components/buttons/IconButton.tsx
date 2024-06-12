610616bff8330fadde8d8cb000e3c3442a6c17b2
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-12 01:08:42 +0100

    Create styling; add tw util classes

--- src/components/buttons/IconButton.tsx
+++ src/components/buttons/IconButton.tsx
@@ -2,7 +2,14 @@
 
 const IconButton = (props: React.ComponentPropsWithRef<"button">) => {
   return (
-    <button {...props} className={"" + " " + props.className}>
+    <button
+      {...props}
+      className={
+        "h-fit px-3 py-2 hover:md:bg-muted-dark rounded hover:md:bg-opacity-75" +
+        " " +
+        props.className
+      }
+    >
       {props.children}
     </button>
   );
