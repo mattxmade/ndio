04c425497265bb6f7a332cf8ea3b161ef4aaf2ad
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-12 21:24:56 +0100

    Create showModal func; implement useRef + useEffect; update props

--- src/components/modal/ModalButton.tsx
+++ src/components/modal/ModalButton.tsx
@@ -1,19 +1,31 @@
 "use client";
 
-import { LucideProps, MoreHorizontalIcon } from "lucide-react";
+import { useEffect, useRef } from "react";
+import IconButton from "../buttons/IconButton";
 
-import IconButton from "./IconButton";
-
-type MenuButtonProps = {
-  iconProps?: LucideProps;
+type ModalButtonProps = {
+  modalId: string;
 } & React.ComponentPropsWithRef<"button">;
 
-const MenuButton = ({ iconProps, ...props }: MenuButtonProps) => {
+const ModalButton = ({ modalId, ...props }: ModalButtonProps) => {
+  const modalRef = useRef<HTMLDialogElement | null>(null);
+
+  useEffect(() => {
+    const modalElement = document.getElementById(modalId) as HTMLDialogElement;
+    if (!modalElement || modalElement.tagName !== "DIALOG") return;
+
+    modalRef.current = modalElement;
+  }, []);
+
+  const showModal = () => {
+    modalRef.current && modalRef.current.showModal();
+  };
+
   return (
-    <IconButton {...props}>
-      <MoreHorizontalIcon {...iconProps} />
+    <IconButton {...props} onClick={showModal}>
+      {props.children}
     </IconButton>
   );
 };
 
-export default MenuButton;
+export default ModalButton;
