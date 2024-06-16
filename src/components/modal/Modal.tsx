58e249a5d75b6528ac87ce9d223b7e641741a7a4
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-16 15:01:16 +0100

    Implement variant Components; handle viewport change logic

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -3,59 +3,86 @@
 import { useState } from "react";
 import { Drawer } from "vaul";
 
-type ModalProps = {
-  id?: string;
-  variant: "drawer" | "popup" | "tooltip";
-  modalBtn?: Omit<React.ComponentPropsWithRef<"button">, "onClick">;
-  children?: React.ReactNode;
+import Tray from "./Tray";
+import Popup from "./Popup";
+import useBreakpoint from "../layout/useBreakpoint";
+
+type Variant = "drawer" | "popup" | "tooltip";
+type View = { view: React.ReactNode; variant: Variant };
+
+type Views = {
+  sm?: View;
+  md?: View;
+  lg?: View;
+  xl?: View;
+  "2xl"?: View;
 };
 
-const Modal = ({ variant, modalBtn, ...props }: ModalProps) => {
-  const [open, setOpen] = useState(false);
+type Trigger = {
+  FC: React.FC<React.ComponentPropsWithRef<"button">>;
+  props?: React.ComponentPropsWithRef<"button">;
+};
 
-  const handleOpen = () => setOpen(true);
+type ModalProps = {
+  id: string;
+  views?: Views;
+  variant: Variant;
+  Trigger: Trigger;
+} & Omit<React.ComponentPropsWithRef<"dialog">, "id">;
+
+const Modal = ({ views, variant, Trigger, ...props }: ModalProps) => {
+  const [open, setOpen] = useState(false);
+  const [type, setType] = useState(variant);
+  const [view, setView] = useState(props.children);
+
+  const { currentBreakpoint } = useBreakpoint();
+
+  const handleOpen = () => {
+    const viewportChange =
+      views && currentBreakpoint && views[currentBreakpoint];
+
+    if (viewportChange) {
+      setType(viewportChange.variant);
+      setView([viewportChange.view]);
+    } else {
+      setType(variant);
+      setView(props.children);
+    }
+
+    setOpen(true);
+  };
   const handleClose = () => setOpen(false);
 
   return (
     <Drawer.Root open={open} onClose={handleClose}>
-      <Drawer.Trigger {...modalBtn} onClick={handleOpen}>
-        {/* {!open ? "Open" : "Closed"} */}
-        {modalBtn?.children}
-      </Drawer.Trigger>
+      <Trigger.FC
+        {...Trigger.props}
+        id={"dialog-" + props.id + "-button"}
+        aria-controls={"dialog-" + props.id}
+        aria-label={props["aria-label"]}
+        onClick={handleOpen}
+      />
       <Drawer.Portal>
         <Drawer.Overlay
           className={`block fixed inset-0 cursor-pointer ${
-            variant !== "tooltip" ? "bg-black/70" : ""
+            type !== "tooltip" ? "bg-black/70" : ""
           }`}
           onClick={handleClose}
           data-state={open ? "open" : "closed"}
         />
 
-        {variant === "popup" ? (
-          <Drawer.Content className={"fixed bottom-0 left-0 right-0"}>
-            <div
-              onClick={handleClose}
-              className={`w-full[100vw] h-[100dvh] grid justify-center items-center duration-300 cursor-pointer`}
-              style={{
-                transform: open
-                  ? "translate3d(0,0,0)"
-                  : "translate3d(0,100%,0)",
-              }}
-            >
-              {props.children}
-            </div>
-          </Drawer.Content>
+        {type === "popup" ? (
+          <Popup
+            id={"dialog-" + props.id}
+            open={open}
+            handleClose={handleClose}
+          >
+            {view}
+          </Popup>
         ) : null}
 
-        {variant === "drawer" ? (
-          <Drawer.Content
-            className={
-              "flex flex-col rounded-t-[10px] max-h-[96%] fixed bottom-0 left-0 right-0"
-            }
-          >
-            <Drawer.Handle />
-            {props.children}
-          </Drawer.Content>
+        {type === "drawer" ? (
+          <Tray id={"dialog-" + props.id}>{view}</Tray>
         ) : null}
       </Drawer.Portal>
     </Drawer.Root>
