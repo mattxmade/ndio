ed889cc1f1a034cd9fd5cc562248110e31e6f59f
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-25 09:54:31 +0100

    Add default styling

--- src/components/core/Button.tsx
+++ src/components/core/Button.tsx
@@ -3,8 +3,12 @@
 const Button = (props: React.ComponentPropsWithRef<"button">) => {
   return (
     <button
-      className="text-sm w-fit py-[0.6rem] px-4 rounded duration-100 bg-splash hover:bg-opacity-80"
       {...props}
+      className={
+        "text-sm w-fit py-[0.6rem] px-4 rounded duration-100 bg-splash hover:bg-opacity-80" +
+          " " +
+          props.className ?? ""
+      }
     />
   );
 };
