1eba4ba654be0288ca92d5adcd258600edf103e6
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-25 11:41:48 +0100

    Complete layout and styling of elements

--- src/components/auth/SignIn.tsx
+++ src/components/auth/SignIn.tsx
@@ -17,35 +17,56 @@ const SignIn = (props: SingInProps) => {
           aria-label="close sign in window"
           className="absolute top-4 right-4"
         >
-          <XIcon />
+          <XIcon size={16} />
         </button>
 
-        <section className="mb-2">
-          <h2 className="text-primary">{signin.heading}</h2>
+        <section className="mb-5 grid justify-center md:justify-start">
+          <h2 className="text-primary text-xl font-bold text-center md:text-left">
+            {signin.heading}
+          </h2>
           <p className="text-xs text-muted">{signin.subheading}</p>
         </section>
 
-        <ul>
-          {signin.providers.map((provider) => (
-            <li key={provider.name} className="py-2">
-              <Button className="px-8 py-1">
-                <provider.Icon /> Sign in with {provider.name}
-              </Button>
-            </li>
-          ))}
-        </ul>
+        <section className="relative grid justify-items-center gap-12 px-5 pt-10 pb-3 rounded-lg border-primary/90 border-4">
+          <ul className="w-fit">
+            {signin.providers.map((provider) => (
+              <li key={provider.name} className="py-2">
+                <Button className="w-60 h-10 flex gap-4 items-center px-8 py-[0] text-background bg-primary/100">
+                  <provider.Icon />
+                  <span className="text-slate-900">
+                    Sign in with {provider.name}
+                  </span>
+                </Button>
+              </li>
+            ))}
+          </ul>
 
-        <p className="text-xs text-primary">
-          {signin.terms.map((content) =>
-            content.link ? (
-              <a key={content.text} href={content.link} className="underline">
-                {content.text}
-              </a>
-            ) : (
-              <Fragment key={content.text}>content.text</Fragment>
-            )
-          )}
-        </p>
+          <p className="relative bottom-2 left-4 text-xs text-primary w-[15.4rem]">
+            {signin.terms.map((content) =>
+              content.link ? (
+                <a key={content.text} href={content.link} className="underline">
+                  {content.text}
+                </a>
+              ) : (
+                <Fragment key={content.text}>{content.text}</Fragment>
+              )
+            )}
+          </p>
+
+          <div
+            className="w-24 h-24 absolute left-0 bottom-0 overflow-hidden grid justify-items-center bg-primary"
+            style={{ clipPath: "polygon(0 50%, 0% 100%, 50% 100%)" }}
+          >
+            <div
+              className="w-12 h-12 absolute bg-background"
+              style={{
+                left: 0,
+                bottom: 0,
+                clipPath: "polygon(0 50%, 50% 100%, 50% 50%)",
+              }}
+            />
+          </div>
+        </section>
       </div>
     </Modal>
   );
