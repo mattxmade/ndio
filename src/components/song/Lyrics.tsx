3aa73e696305ebd554d4652f8e45019e1e58b2c6
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-25 23:53:23 +0100

    Implement Modal, create Section Cmpnt; progress layout

--- src/components/song/Lyrics.tsx
+++ src/components/song/Lyrics.tsx
@@ -1,26 +1,70 @@
+"use client";
+
 import { Fragment } from "react";
 
-type LyricsProps = {
-  songLyrics: string;
-} & React.ComponentPropsWithRef<"section">;
+import Modal from "../modal/Modal";
+import { ButtonIcon } from "../buttons/IconButton";
+import { ExpandIcon } from "lucide-react";
+import useBreakpoint from "../layout/useBreakpoint";
 
-const Lyrics = ({ songLyrics, ...props }: LyricsProps) => {
-  const lyrics = songLyrics.split("/n");
+type Props = React.ComponentPropsWithRef<"button">;
+
+const Lyrics = ({ ...props }: Props) => {
+  const { currentBreakpoint } = useBreakpoint();
 
   return (
     <Fragment>
-      {lyrics.map((line, i) => (
-        <p
-          key={line + "_lyric-line " + i}
-          className={`text-xs lg:text-base my-2 ${
-            line.endsWith("]") ? "mt-4 font-bold" : ""
-          }`}
+      {currentBreakpoint === "sm" ? (
+        <Modal
+          id={"song-lyrics"}
+          variant="drawer"
+          Trigger={{
+            props: {
+              ...props,
+              className:
+                "w-full h-[186px] p-4 overflow-hidden rounded border border-primary/20",
+              children: (
+                <Fragment>
+                  <div className="w-full flex justify-between">
+                    <h2 className="text-lg md:text-xl font-bold mb-4">
+                      Lyrics
+                    </h2>
+
+                    <ButtonIcon
+                      aria-label="Expand lyrics"
+                      className="block md:hidden"
+                    >
+                      <ExpandIcon />
+                    </ButtonIcon>
+                  </div>
+
+                  <section className="text-sm text-left">
+                    {props.children}
+                  </section>
+                </Fragment>
+              ),
+            },
+
+            FC: Section,
+          }}
         >
-          {line}
-        </p>
-      ))}
+          <section className="text-3xl bg-background">
+            <h2 className="text-lg md:text-xl font-bold mb-4">Lyrics</h2>
+
+            {props.children}
+          </section>
+        </Modal>
+      ) : (
+        <section className="md:w-1/2 bg-background">
+          <h2 className="text-lg md:text-xl font-bold mb-4">Lyrics</h2>
+
+          {props.children}
+        </section>
+      )}
     </Fragment>
   );
 };
+
+const Section = ({ ...props }: Props) => <section {...props} />;
 
 export default Lyrics;
