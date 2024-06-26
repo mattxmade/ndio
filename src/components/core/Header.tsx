a0e60b36e3946d529eba822779521d85cf92594b
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-27 00:07:23 +0100

    Implement GeneratorForm + Button Components; progress layout

--- src/components/core/Header.tsx
+++ src/components/core/Header.tsx
@@ -1,5 +1,28 @@
+import { MenuIcon } from "lucide-react";
+
+import Button from "./Button";
+import GeneratorForm from "../generator/GeneratorForm";
+
 const Header = (props: React.ComponentProps<"header">) => {
-  return <header {...props}>Header</header>;
+  return (
+    <header
+      {...props}
+      className={`fixed md:sticky h-[82px] md:h-fit grid md:flex gap-6 items-center w-full top-0 z-50 bg-background`}
+    >
+      <GeneratorForm />
+
+      <section className="w-fit flex gap-8 justify-self-end md:justify-self-auto">
+        <div className="flex gap-6">
+          <Button className="bg-primary/20">Sign In</Button>
+          <Button>Sign Up</Button>
+        </div>
+
+        <button aria-label="site-map menu">
+          <MenuIcon size={24} className="stroke-primary" />
+        </button>
+      </section>
+    </header>
+  );
 };
 
 export default Header;
