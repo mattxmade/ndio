b1d3fc2ef1200bae3f5e3ebc0f231b4edac05ab1
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-30 22:14:38 +0100

    Implement Player Component; provision boolean state

--- src/components/player/PlayerProvider.tsx
+++ src/components/player/PlayerProvider.tsx
@@ -1,10 +1,14 @@
+"use client";
+
 import React, { createContext, useContext, useState } from "react";
+import Player from "../Player";
 
 type ProviderProps = {
   children: React.ReactNode;
 };
 
 type PlayerContext = {
+  open?: boolean;
   controls: number;
   setControls: React.Dispatch<React.SetStateAction<number>>;
 };
@@ -12,16 +16,19 @@ type PlayerContext = {
 export const PlayerContext = createContext<PlayerContext | null>(null);
 
 export default function PlayerContextProvider({ children }: ProviderProps) {
+  const [open, setOpen] = useState(false);
   const [controls, setControls] = useState(0);
 
   return (
     <PlayerContext.Provider
       value={{
+        open,
         controls,
         setControls,
       }}
     >
       {children}
+      <Player open={open} />
     </PlayerContext.Provider>
   );
 }
