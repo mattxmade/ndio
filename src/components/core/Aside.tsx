ab944fa48516e719717524ada04bf10edda90deb
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-30 22:10:38 +0100

    Implement usePlayerContext hook; set conditional height via player open state

--- src/components/core/Aside.tsx
+++ src/components/core/Aside.tsx
@@ -1,10 +1,4 @@
-import {
-  HomeIcon,
-  MusicIcon,
-  HeartIcon,
-  ListMusicIcon,
-  UserIcon,
-} from "lucide-react";
+"use client";
 
 import {
   TwitterIcon,
@@ -13,17 +7,25 @@ import {
   TikTokIcon,
   RedditIcon,
 } from "../Icons";
+import { usePlayerContext } from "../player/PlayerProvider";
 
 const Aside = (props: React.ComponentProps<"aside">) => {
+  const player = usePlayerContext();
+
+  const height = !player.open ? "h-screen" : "h-[calc(100vh-6rem)]";
+
   return (
-    <aside {...props}>
+    <aside
+      {...props}
+      className={`relative md:sticky top-0 pt-4 h-fit md:${height} bg-background`}
+    >
       <a href="/">
         <img src="/logo.svg" alt="logo" className="w-40 pt-1 pl-5" />
       </a>
 
       {props.children}
 
-      <footer className="absolute hidden md:visible w-full p-2 bottom-24 md:flex flex-col items-center gap-4 bg-background">
+      <footer className="absolute hidden md:visible w-full p-2 bottom-0 md:flex flex-col items-center gap-4 bg-background">
         <p className="text-xs text-muted">AI Music Generator</p>
 
         <div className="flex gap-4">
