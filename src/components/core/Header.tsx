2f0283dc20a7481bc9a564cab61997ee95497389
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-30 19:10:35 +0100

    Update view layout

--- src/components/core/Header.tsx
+++ src/components/core/Header.tsx
@@ -7,14 +7,14 @@ const Header = (props: React.ComponentProps<"header">) => {
   return (
     <header
       {...props}
-      className={`fixed md:sticky h-[82px] md:h-fit grid md:flex gap-6 items-center w-full top-0 z-50 bg-background`}
+      className={`fixed md:sticky w-full h-[82px] md:h-fit top-0 z-50 grid md:flex gap-6 items-center   bg-background`}
     >
       <GeneratorForm />
 
-      <section className="w-fit flex gap-8 justify-self-end md:justify-self-auto">
+      <section className="w-fit pl-2 pr-4 md:px-0 flex gap-8 justify-self-end md:justify-self-auto">
         <div className="flex gap-6">
           <Button className="bg-primary/20">Sign In</Button>
-          <Button>Sign Up</Button>
+          <Button className="bg-splash">Sign Up</Button>
         </div>
 
         <button aria-label="site-map menu">
