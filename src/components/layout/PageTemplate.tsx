a7f2d29febbc8fc81ea1ced997cf0aec31304fb4
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-30 22:13:30 +0100

    Move class utils to Component level

--- src/components/layout/PageTemplate.tsx
+++ src/components/layout/PageTemplate.tsx
@@ -10,7 +10,7 @@ const PageTemplate = ({ variant, children }: PageTemplateProps) => (
   <div
     className={`relative flex flex-col md:flex-row gap-1 md:pl-2 md:pr-4 min-h-screen`}
   >
-    <Aside className="relative md:sticky top-0 pt-4 h-fit md:h-screen bg-background">
+    <Aside>
       <Nav className={variant ? "hidden md:flex" : ""} />
     </Aside>
     <section className="flex-auto">
