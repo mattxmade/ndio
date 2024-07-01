6498568146d9a4dd443238720e4c5d47a2834398
Matt <97047080+mattxmade@users.noreply.github.com>
2024-07-01 01:27:26 +0100

    Update type defintions; update controls state

--- src/components/player/PlayerProvider.tsx
+++ src/components/player/PlayerProvider.tsx
@@ -1,23 +1,37 @@
 "use client";
 
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useCallback, useContext, useState } from "react";
 import Player from "../Player";
 
 type ProviderProps = {
   children: React.ReactNode;
 };
 
+type Controls = {
+  play: boolean;
+  prev: boolean;
+  next: boolean;
+  shuffle: boolean;
+  repeat: boolean;
+};
+
 type PlayerContext = {
   open?: boolean;
-  controls: number;
-  setControls: React.Dispatch<React.SetStateAction<number>>;
+  controls: Controls;
+  setControls: React.Dispatch<React.SetStateAction<Controls>>;
 };
 
 export const PlayerContext = createContext<PlayerContext | null>(null);
 
 export default function PlayerContextProvider({ children }: ProviderProps) {
   const [open, setOpen] = useState(false);
-  const [controls, setControls] = useState(0);
+  const [controls, setControls] = useState<PlayerContext["controls"]>({
+    play: false,
+    prev: false,
+    next: false,
+    shuffle: false,
+    repeat: false,
+  });
 
   return (
     <PlayerContext.Provider
