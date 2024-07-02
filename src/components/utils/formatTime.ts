1b6e699cb7795bc5ef932980bb50c0bda63aeb3a
Matt <97047080+mattxmade@users.noreply.github.com>
2024-07-03 00:17:13 +0100

    Implement date-fns func; calc mins/secs; return formatted time

--- src/components/utils/formatTime.ts
+++ src/components/utils/formatTime.ts
@@ -1,1 +1,12 @@
-const formatTime = (seconds: number) => {};
+import { secondsToMinutes } from "date-fns";
+
+const formatTime = (timeInSeconds: number) => {
+  const mins = secondsToMinutes(timeInSeconds);
+  const secs = +(timeInSeconds % 60).toFixed();
+
+  return `${secs === 60 ? mins + 1 : mins}:${
+    secs <= 9 ? "0" + secs : secs === 60 ? "00" : secs
+  }`;
+};
+
+export default formatTime;
