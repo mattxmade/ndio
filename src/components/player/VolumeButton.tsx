ed3a21b69f0d90ba07eb9632c9e1fa102b8369b7
Matt <97047080+mattxmade@users.noreply.github.com>
2024-07-05 21:30:35 +0100

    Implement button element onClick handler func; provision state change

--- src/components/player/VolumeButton.tsx
+++ src/components/player/VolumeButton.tsx
@@ -6,9 +6,14 @@ import { Volume2Icon, VolumeXIcon } from "lucide-react";
 const VolumeButton = () => {
   const [muted, setMuted] = useState(false);
 
+  const handleOnClick = () => {
+    setMuted(!muted);
+  };
+
   <button
     aria-label="Volume"
     className="flex justify-center items-center bg-background w-11 h-9 rounded hover:bg-white hover:bg-opacity-20"
+    onClick={handleOnClick}
   >
     {!muted ? (
       <Volume2Icon className="w-5 stroke-primary" />
