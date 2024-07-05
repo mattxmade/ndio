074239612591224737cc939f618f12971ec1871f
Matt <97047080+mattxmade@users.noreply.github.com>
2024-07-05 22:06:45 +0100

    Implement callback function; define Component types

--- src/components/player/MuteButton.tsx
+++ src/components/player/MuteButton.tsx
@@ -3,24 +3,31 @@
 import { useState } from "react";
 import { Volume2Icon, VolumeXIcon } from "lucide-react";
 
-const MuteButton = () => {
+type MuteButtonProps = {
+  onMuteVolume: (muted: boolean) => void;
+};
+
+const MuteButton = (props: MuteButtonProps) => {
   const [muted, setMuted] = useState(false);
 
   const handleOnClick = () => {
+    props.onMuteVolume(!muted);
     setMuted(!muted);
   };
 
-  <button
-    aria-label="Volume"
-    className="flex justify-center items-center bg-background w-11 h-9 rounded hover:bg-white hover:bg-opacity-20"
-    onClick={handleOnClick}
-  >
-    {!muted ? (
-      <Volume2Icon className="w-5 stroke-primary" />
-    ) : (
-      <VolumeXIcon className="w-5 stroke-primary" />
-    )}
-  </button>;
+  return (
+    <button
+      aria-label="Volume"
+      className="flex justify-center items-center bg-background w-11 h-9 rounded hover:bg-white hover:bg-opacity-20"
+      onClick={handleOnClick}
+    >
+      {!muted ? (
+        <Volume2Icon className="w-5 stroke-primary" />
+      ) : (
+        <VolumeXIcon className="w-5 stroke-primary" />
+      )}
+    </button>
+  );
 };
 
 export default MuteButton;
