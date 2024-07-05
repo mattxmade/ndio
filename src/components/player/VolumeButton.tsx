3cc1f2350f824725cbbe813c5e0d27235dbeaeaf
Matt <97047080+mattxmade@users.noreply.github.com>
2024-07-05 21:26:36 +0100

    Implement useState hook; provision lucide Cmpnt render condition

--- src/components/player/VolumeButton.tsx
+++ src/components/player/VolumeButton.tsx
@@ -1,13 +1,20 @@
 "use client";
 
-import { Volume2Icon } from "lucide-react";
+import { useState } from "react";
+import { Volume2Icon, VolumeXIcon } from "lucide-react";
 
 const VolumeButton = () => {
+  const [muted, setMuted] = useState(false);
+
   <button
     aria-label="Volume"
     className="flex justify-center items-center bg-background w-11 h-9 rounded hover:bg-white hover:bg-opacity-20"
   >
-    <Volume2Icon className="w-5 stroke-primary" />
+    {!muted ? (
+      <Volume2Icon className="w-5 stroke-primary" />
+    ) : (
+      <VolumeXIcon className="w-5 stroke-primary" />
+    )}
   </button>;
 };
 
