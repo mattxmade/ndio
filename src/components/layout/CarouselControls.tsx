0e4e216e9ec373a5e7a954ad6fbd7e5a5c945323
Matt <97047080+mattxmade@users.noreply.github.com>
2024-07-13 13:11:14 +0100

    Add Carousel buttons; implement useIntersection; provision navigation logic

---
+++ src/components/layout/CarouselControls.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import { ArrowLeftIcon } from "lucide-react";
+import { useIntersection } from "../intersection/useIntersection";
+
+const CarouselControls = ({ id }: { id: string }) => {
+  const carouselContRef = useRef<HTMLElement | null>(null);
+
+  const firstItemRef = useRef<Element | null>(null);
+  const lastItemRef = useRef<Element | null>(null);
+
+  const firstItemObserver = useIntersection({
+    root: carouselContRef.current,
+  });
+  const lastItemObserver = useIntersection({
+    root: carouselContRef.current,
+  });
+
+  const [navPrevActive, setNavPrevActive] = useState(false);
+  const [navNextActive, setNavNextActive] = useState(true);
+
+  useEffect(() => {
+    const carouselCont = document.getElementById(id);
+
+    if (!carouselCont) return;
+
+    carouselContRef.current = carouselCont;
+    const items = carouselCont.getElementsByClassName("carousel-item");
+
+    if (!items.length) return;
+
+    const firstItem = items[0];
+    const lastItem = items[items.length - 1];
+
+    firstItem && (firstItemRef.current = firstItem);
+    lastItem && (lastItemRef.current = lastItem);
+
+    firstItemObserver.ref(firstItemRef.current);
+    lastItemObserver.ref(lastItemRef.current);
+  }, []);
+
+  useEffect(() => {
+    !firstItemObserver.entry?.isIntersecting
+      ? setNavPrevActive(true)
+      : setNavPrevActive(false);
+
+    !lastItemObserver.entry?.isIntersecting
+      ? setNavNextActive(true)
+      : setNavNextActive(false);
+  }, [firstItemObserver.entry, lastItemObserver.entry]);
+
+  return (
+    <>
+      <button
+        className="p-2 rounded-ful bg-opacity-0 duration-300 hover:bg-opacity-15"
+        aria-label="navigate left"
+      >
+        <ArrowLeftIcon
+          size={24}
+          className={`${navPrevActive ? "stroke-primary" : "stroke-muted"}`}
+        />
+      </button>
+      <button
+        className="rotate-180 p-2 rounded-full bg-opacity-0 duration-300 hover:bg-opacity-15"
+        aria-label="navigate right"
+      >
+        <ArrowLeftIcon
+          size={24}
+          className={`${navNextActive ? "stroke-primary" : "stroke-muted"}`}
+        />
+      </button>
+    </>
+  );
+};
+
+export default CarouselControls;
