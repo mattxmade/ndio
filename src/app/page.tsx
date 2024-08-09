4a1737771804248e9b180185d5f1235a719d23b4
Matt <97047080+mattxmade@users.noreply.github.com>
2024-08-09 22:20:41 +0100

    Implement Components: Carousel, SortOptions + Search; update layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,52 @@
-import Carousel from "@/components/layout/Carousel";
 import Search from "@/components/query/Search";
+import Carousel from "@/components/layout/Carousel";
 import SortOptions from "@/components/query/SortOptions";
+import PageTemplate from "@/components/layout/PageTemplate";
 import { generatePlaylist, songGroups } from "@/content";
 
 const moreTracks = generatePlaylist(songGroups.moreTracks.numOfSongs);
 
 export default function Home() {
   return (
-    <main className="grid gap-6 mt-3 mb-36 pt-3 md:pr-3 pb-7 pl-[1.4rem] rounded bg-background">
-      {songGroups.categories.map((category) => (
+    <PageTemplate>
+      <main className="grid gap-6 mt-3 mb-36 pt-3 md:pr-3 pb-7 rounded bg-background">
+        {songGroups.categories.map((category) => (
+          <Carousel
+            key={category.id}
+            id={`carousel-${category.id}`}
+            categoryData={category}
+            className="grid gap-2 pl-[1.4rem]"
+          >
+            <h2 className="text-2xl text-primary text-opacity-90">
+              {category.title}
+            </h2>
+          </Carousel>
+        ))}
+
         <Carousel
-          key={category.id}
-          categoryData={category}
-          className="grid gap-2"
+          id={`carousel-${songGroups.topTracks.id}`}
+          categoryData={songGroups.topTracks}
+          className="hidden md:grid gap-2 pl-[1.4rem]"
         >
-          <h2 className="text-2xl text-primary text-opacity-90">
-            {category.title}
+          <h2 className="w-full text-2xl text-primary text-opacity-90">
+            {songGroups.topTracks.title}
           </h2>
+          <SortOptions
+            sortBy={[
+              { item: "Today" },
+              { item: "This week", initSelect: true },
+              { item: "This month" },
+              { item: "All time" },
+            ]}
+          />
         </Carousel>
-      ))}
 
-      <Carousel categoryData={songGroups.topTracks} className="grid gap-2">
-        <h2 className="w-full text-2xl text-primary text-opacity-90">
-          {songGroups.topTracks.title}
-        </h2>
-        <SortOptions
-          sortBy={[
-            { item: "Today" },
-            { item: "This week", initSelect: true },
-            { item: "This month" },
-            { item: "All time" },
-          ]}
-        />
-      </Carousel>
-
-      <Search items={moreTracks} className="grid gap-2">
-        <h2 className="w-full text-2xl text-primary text-opacity-90">
-          {songGroups.moreTracks.title}
-        </h2>
-      </Search>
-    </main>
+        <Search items={moreTracks} className="grid gap-2 md:pl-[1.4rem]">
+          <h2 className="w-full text-2xl text-primary text-opacity-90">
+            {songGroups.moreTracks.title}
+          </h2>
+        </Search>
+      </main>
+    </PageTemplate>
   );
 }
